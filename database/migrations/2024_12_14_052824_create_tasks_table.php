83a1aacb75aace30076c5ddea99cdbf549b12f10
mohitjain1911 <mohitjain19113@gmail.com>
2024-12-14 11:18:52 +0530

    Initial

--- database/migrations/2024_12_14_052824_create_tasks_table.php
+++ database/migrations/2024_12_14_052824_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('priority');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
